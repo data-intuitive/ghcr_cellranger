bab3eb44296f691296c7a4c3b25ebe79be8cfc4c
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-21 22:51:56 +0100

    only build one

--- src/cellranger/download.sh
+++ src/cellranger/download.sh
@@ -17,7 +17,8 @@ for tag in 1.0 1.1 1.2 1.3 2.0 2.1 2.2 3.0 3.1 4.0 5.0 6.0 6.1; do
     echo "Building image for Cell Ranger $tag"
     bin/viash run src/cellranger/build_cellranger/config.vsh.yaml -- \
       --input "$zip" \
-      --tag "ghcr.io/data-intuitive/cellranger:$tag"
+      --tag "ghcr.io/data-intuitive/cellranger:$tag" \
+      --push
 
     exit 1
 done
