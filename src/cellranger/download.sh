48fb2a75bb5d2cad3cb31b96c3f59923007d8bfc
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-22 12:08:37 +0100

    remove encryption

--- src/cellranger/download.sh
+++ src/cellranger/download.sh
@@ -8,16 +8,10 @@ fi
 
 for tag in 6.1 6.0 5.0 4.0 3.1 3.0 2.2 2.1 2.0 1.3 1.2 1.1 1.0; do
     tar_gz="output/cellranger/cellranger_$tag.tar.gz"
-    zip="output/cellranger/cellranger_$tag.zip"
 
     if [ ! -f "$tar_gz" ]; then
         echo "Downloading Cell Ranger $tag"
         bin/viash run src/cellranger/download_cellranger/config.vsh.yaml -- --tag $tag --output "$tar_gz"
-    fi
-
-    if [ ! -f "$zip" ]; then
-        echo "Encrypting Cell Ranger $tag"
-        bin/viash run src/cellranger/encrypt_cellranger/config.vsh.yaml -- --input "$tar_gz" --password foo --output "$zip"
     fi
 
     echo "Building image for Cell Ranger $tag"
@@ -28,7 +22,7 @@ for tag in 6.1 6.0 5.0 4.0 3.1 3.0 2.2 2.1 2.0 1.3 1.2 1.1 1.0; do
     fi
 
     bin/viash run src/cellranger/build_cellranger/config.vsh.yaml -- \
-      --input_cellranger "$zip" \
+      --input_cellranger "$tar_gz" \
       --input_bcl2fastq "$bcl2fastq_zip" \
       --tag "ghcr.io/data-intuitive/cellranger:$tag$latest_tag" # \
     #   --push
