14a5b6be125b2b71429312525deae3ef1d35efcf
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-22 16:14:14 +0100

    update

--- src/cellranger/download.sh
+++ src/cellranger/download.sh
@@ -24,8 +24,8 @@ for tag in 6.1 6.0 5.0 4.0 3.1 3.0 2.2 2.1 2.0 1.3 1.2 1.1 1.0; do
     bin/viash run src/cellranger/build_cellranger/config.vsh.yaml -- \
       --input_cellranger "$tar_gz" \
       --input_bcl2fastq "$bcl2fastq_zip" \
-      --tag "ghcr.io/data-intuitive/cellranger:$tag$latest_tag" # \
-    #   --push
+      --tag "ghcr.io/data-intuitive/cellranger:$tag$latest_tag" \
+      --push
 
     exit 1
 done
