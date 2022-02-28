4863e81f6ee4c1da0b990c5ba5fe90d91d9a48bc
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-28 14:27:41 +0100

    add cellranger arc

---
+++ src/cellranger_arc/build_cellranger_arc/script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## VIASH START
+par_input_cellranger_arc='cellranger_arc.tar.gz'
+par_input_bcl2fastq='bcl2fastq.zip'
+par_tag='ghcr.io/data-intuitive/cellranger_arc:latest'
+meta_functionality_name='build_cellranger_arc'
+## VIASH END
+
+tempdir=$(mktemp -d "$VIASH_TEMP/run-${meta_functionality_name}-XXXXXX")
+if [[ ! "$tempdir" || ! -d "$tempdir" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+function cleanup {      
+  rm -rf "$tempdir"
+  echo "Deleted temp working directory $tempdir"
+}
+trap cleanup EXIT
+
+cp "$meta_resources_dir/Dockerfile" "$tempdir/Dockerfile"
+cp "$par_input_cellranger_arc" "$tempdir/cellranger_arc.tar.gz"
+
+unzip "$par_input_bcl2fastq" -d "$tempdir"
+rpm_name=`ls -1 "$tempdir" | grep rpm`
+mv "$tempdir/$rpm_name" "$tempdir/bcl2fastq.rpm"
+
+docker build -t "$meta_functionality_name" "$tempdir"
+
+if [ ! -z "$par_tag" ]; then
+  IFS=","
+  for var in $par_tag; do
+    echo "Tagging $var"
+    unset IFS
+    docker tag "$meta_functionality_name" "$var"
+
+    if [ "$par_push" == "true" ]; then
+      echo "Pushing $var"
+      docker push "$var"
+    fi
+  done
+fi
---
+++ src/cellranger_arc/download_cellranger_arc/script.py
@@ -0,0 +1,95 @@
+import random
+import time
+import os
+import re
+import tempfile
+import shutil
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import sys
+
+## VIASH START
+par = {
+  'tag': 'latest',
+  'timeout': 600,
+  'output': 'cellranger_arc.tar.gz',
+  'multiplier': 1.0
+}
+## VIASH END
+
+url = f"https://support.10xgenomics.com/single-cell-multiome-atac-gex/software/downloads/{par['tag']}"
+
+def sleep(x):
+    time.sleep(x * par['multiplier'])
+
+with tempfile.TemporaryDirectory() as download_dir:
+    print("Opening Firefox", flush=True)
+    options = webdriver.firefox.options.Options()
+    options.headless = True
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.download.dir", download_dir)
+    options.set_preference("browser.helperApps.alwaysAsk.force", False)
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip,application/x-gzip,application/x-tar")
+
+    driver = webdriver.Firefox(options=options)
+
+    def random_keys():
+        return "".join([random.choice('qwertzuiopasdfghjklyxcvbnm') for _ in range(random.randrange(5, 10))])
+
+    sleep(5)
+
+    print("Navigating to form", flush=True)
+    driver.get(url)
+
+    sleep(5)
+
+    # Fill out form
+    form = driver.find_element(By.ID, 'eula-form')
+    sleep(.1)
+    form.find_element(By.ID, 'first_name').send_keys(random_keys())
+    sleep(.1)
+    form.find_element(By.ID, 'last_name').send_keys(random_keys())
+    sleep(.1)
+    form.find_element(By.ID, 'email').send_keys(random_keys() + '@gmail.com')
+    sleep(.1)
+    form.find_element(By.ID, 'company').send_keys(random_keys())
+    sleep(.1)
+    agree = form.find_element(By.ID, "agree")
+    if not agree.is_selected():
+        agree.click()
+
+    assert agree.is_selected()
+
+    # Go to download page
+    print("Navigating to download page", flush=True)
+    form.submit()
+    sleep(5)
+
+    # Download cellranger_arc
+    print("Downloading Cell Ranger ARC", flush=True)
+    elem = driver.find_element(By.PARTIAL_LINK_TEXT, "Download - Linux")
+    elem.click()
+    url = elem.get_property("href")
+    filename = re.sub("^.*/([^?/]*)?[^/]*$", "\\1", url)
+    dest_path = os.path.join(download_dir, filename)
+    sleep(3)
+
+    # Wait until file is completely downloaded before exiting
+    i = 0
+    while i < par["timeout"] and os.path.exists(dest_path + ".part"):
+        sleep(3)
+        print("Content of download dir: " + ', '.join(os.listdir(download_dir)), flush=True)
+        i += 1
+
+    print("Quitting firefox", flush=True)
+    driver.quit()
+
+    if not os.path.exists(dest_path):
+        raise FileNotFoundError(f"Download has not completed after {par['timeout']}s. Is this script still working?")
+
+    print(f"Copying {dest_path} to {par['output']}", flush=True)
+    shutil.copy(dest_path, par["output"])
+
+    print("Download complete", flush=True)
+    sys.stdout.flush()
